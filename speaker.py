67e517852a5be53c9962657a652067a441b8fc06
Tony Stark <122863075+Neela-krishna-5833@users.noreply.github.com>
2024-05-16 07:51:42 +0530

    Update speaker.py
    Added comments to explain the double imports

--- speaker.py
+++ speaker.py
@@ -1,11 +1,11 @@
-import gtts as gTTS
-import os
-import os
-from time import sleep
-import requests
-from math import floor
-from datetime import datetime
-from gtts import gTTS
+import gtts as gTTS                                       ###############################################################################
+import os                                                 ###############################################################################
+import os                                                 ###############################################################################
+from time import sleep                                    ##########################I don't know why the hell should I###################
+import requests                                           ##########################Import some modules twice. If I remove###############
+from math import floor                                    ##########################the duplicates, I am getting errors...###############
+from datetime import datetime                             ###############################################################################
+from gtts import gTTS                                     ###############################################################################
 
 def speaker(variable_text):
     tts = gTTS(variable_text)
