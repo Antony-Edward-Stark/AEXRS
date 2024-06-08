def0c0379abbf6ca35ca764e799679e4568807f0
Tony Stark <m.neelakrishna5833@gmail.com>
2024-06-09 00:27:48 +0530

    Added the ability to get lyrics of a song

--- functions.py
+++ functions.py
@@ -1,9 +1,13 @@
 import os
 import requests
+import re
+import urllib.request
 from math import floor
 from datetime import datetime
+from bs4 import BeautifulSoup
 from speaker import speaker
 from platform import system
+
 
 # from AppOpener import open
 
@@ -180,3 +184,36 @@ def maintenance_tasks():
 
     elif system() == "Windows":
         pass
+
+
+def get_lyrics(artist, song_title):
+    # ================================================================================================= #
+    # ========================================= For Lyrics ============================================ #
+    # ================================================================================================= #
+    artist = artist.lower()
+    song_title = song_title.lower()
+
+    artist = re.sub("[^A-Za-z0-9]+", "", artist)
+    song_title = re.sub("[^A-Za-z0-9]+", "", song_title)
+
+    if artist.startswith("the"):
+        artist = artist[3:]
+    
+    url = "http://azlyrics.com/lyrics/" + artist + "/" + song_title + ".html"
+
+    try:
+        content = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(content, "html.parser")
+        lyrics = str(soup)
+        
+        up_partition = "<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
+        down_partition = "<!-- MxM banner -->"
+        lyrics = lyrics.split(up_partition)[1]
+        lyrics = lyrics.split(down_partition)[0]
+        lyrics = lyrics.replace("</br>", " ")
+        lyrics = lyrics.replace("<br/>", " ")
+
+        print(lyrics)
+
+    except Exception as e:
+        return "Exception occurred \n" + str(e)
